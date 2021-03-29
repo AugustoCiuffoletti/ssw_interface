bb1e40690ba84be65f10b323c413a640a09145a8
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-29 11:54:26 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 interface mioTipo {
-    f1: string;
-    f2: number
-};
+  f1: string;
+  f2: number;
+  op: (arg1: number) => number;
+}
 var X: mioTipo = {
-    f1:"",
-    f2:0
+  f1: "",
+  f2: 0,
+  op: function(x) {
+    return x + this.f2;
+  }
 };
 X.f1 = "Hello";
 X.f2 = 2;
 console.log(X);
+console.log(X.op(7));
+X.op = function(a) {
+  return a * this.f2;
+};
+console.log(X.op(7));
